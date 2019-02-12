394277536ade219ca810554ea07cf7090f81b60c
levara <hrvoje.leventic@gmail.com>
2019-02-12 22:08:19 +0100

    Add changing bg color on space keypress

--- test.py
+++ test.py
@@ -6,6 +6,12 @@ import sys
 WIDTH = 800
 HEIGHT = 600
 
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
 
 # inicijalizacija pygame sustava
 pygame.init()
@@ -17,13 +23,38 @@ pygame.display.set_caption("LABUS igraa!")
 
 clock = pygame.time.Clock()
 
+game_state = "start"
+next_state = False
 done = False
 while not done:
+    #event petlja
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                next_state = True 
+    #Provjera i promjena stanja
+    if next_state:
+        next_state = False
+        if game_state == "start":
+            game_state = "red"
+        elif game_state == "red":
+            game_state = "green"
+        elif game_state == "green":
+            game_state = "blue"
+        elif game_state == "blue":
+            game_state = "red"
 
-    screen.fill( (255,0,0) )
+    #Izmjena iscrtavanja na ekranu prema odabranom stanju
+    if game_state == "start":
+        screen.fill( WHITE )
+    elif game_state == "red":
+        screen.fill( RED )
+    elif game_state == "green":
+        screen.fill( GREEN )
+    elif game_state == "blue":
+        screen.fill( BLUE )
 
     pygame.display.flip()
     clock.tick(60)
