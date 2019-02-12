2d4b5132f4f5c9b8d93f088e0ec17554f878dab0
levara <hrvoje.leventic@gmail.com>
2019-02-12 22:19:26 +0100

    Add welcome text and welcome image

--- test.py
+++ test.py
@@ -12,11 +12,16 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+welcome_image = pygame.image.load("radionica-petak/shark.jpg")
 
 # inicijalizacija pygame sustava
 pygame.init()
 
 pygame.font.init()
+
+#Odabir fonta i kreiranje pozdravnog teksta
+myfont = pygame.font.SysFont('Arial', 40)
+welcome_text = myfont.render("Welcomeee!", False, BLACK)
 
 screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption("LABUS igraa!")
@@ -34,6 +39,8 @@ while not done:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 next_state = True 
+            if event.key == pygame.K_q:
+                done = True
     #Provjera i promjena stanja
     if next_state:
         next_state = False
@@ -49,6 +56,8 @@ while not done:
     #Izmjena iscrtavanja na ekranu prema odabranom stanju
     if game_state == "start":
         screen.fill( WHITE )
+        screen.blit( welcome_image, (0,0))
+        screen.blit( welcome_text, (WIDTH/2, HEIGHT/2))
     elif game_state == "red":
         screen.fill( RED )
     elif game_state == "green":
